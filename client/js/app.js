8c58227a8b64a2746c14df64e56c10853f4b75c8
Sahil Bajaj <sahil29@gmail.com>
2015-02-08 14:27:07 +0800

    [client] Initial commit of client code
    - Using react; build using `watchify -vo build/js/app.js js/app.js`

---
+++ client/js/app.js
@@ -0,0 +1,53 @@
+var React = require('react');
+var qwest = require('qwest')
+
+// var url = 'https://api.github.com/repos/spinningarrow/spinningarrow.github.io/contents/_posts';
+var url = 'temp-posts.json';
+
+var PostBox = React.createClass({
+	getInitialState: function () {
+		return { data: [] };
+	},
+
+	componentDidMount: function () {
+		qwest.get(url)
+			.then(function (response) {
+				// console.log(response);
+				console.log('posts gotten');
+				this.setState({ data: JSON.parse(response) });
+			}.bind(this));
+	},
+
+	render: function () {
+		var array = [ 1, 2, 3, 4 ];
+		return (
+			<div className='posts-box'>
+				<PostList posts={this.state.data}/>
+			</div>
+		);
+	}
+});
+
+var PostList = React.createClass({
+	render: function () {
+		var nodes = this.props.posts.map(function (post) {
+			return (
+				<li><Post title={post.name} /></li>
+			);
+		});
+
+		return (
+			<ul>{nodes}</ul>
+		);
+	}
+});
+
+var Post = React.createClass({
+	render: function () {
+		return (
+			<div>{this.props.title}</div>
+		);
+	}
+});
+
+React.render(<PostBox/>, document.querySelector('main'));
