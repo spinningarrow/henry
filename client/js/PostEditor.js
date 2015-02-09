0da3678ed638030563002eee7b74615049dea079
Sahil Bajaj <sahil29@gmail.com>
2015-02-10 01:35:39 +0800

    [PostEditor] Add basic post editor component
    - It doesn't do anything (yet)

---
+++ client/js/PostEditor.js
@@ -0,0 +1,13 @@
+var React = require('react');
+
+var PostEditor = React.createClass({
+	render: function () {
+		return (
+			<div className="post-editor">
+				<textarea></textarea>
+			</div>
+		);
+	}
+});
+
+module.exports = PostEditor;
--- client/js/app.js
+++ client/js/app.js
@@ -4,5 +4,7 @@ var React = require('react');
 var Post = require('./Post');
 var PostList = require('./PostList');
 var PostBox = require('./PostBox');
+var PostEditor = require('./PostEditor');
 
-React.render(<PostBox/>, document.querySelector('main'));
+React.render(<PostBox/>, document.querySelector('#posts'));
+React.render(<PostEditor/>, document.querySelector('#post-editor'));
