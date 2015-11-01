bdac40c087e60fe7f26535709ca7f6f3e3c9000a
Sahil Bajaj <sahil29@gmail.com>
2015-11-02 00:09:15 +0800

    Remove unused requires from app.js

--- client/js/app.js
+++ client/js/app.js
@@ -1,10 +1,6 @@
 require('normalize-css');
 var React = require('react');
 var ReactDOM = require('react-dom');
-
-var Post = require('./Post');
-var PostList = require('./PostList');
 var PostBox = require('./PostBox');
-var PostEditor = require('./PostEditor');
 
 ReactDOM.render(<PostBox/>, document.querySelector('main'));
