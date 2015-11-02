5899454bfcf6ec0ab1e50f42b9a57f5772de63f6
Sahil Bajaj <sahil29@gmail.com>
2015-11-02 21:48:55 +0800

    Convert PostList to stateless functional component
    - This is pretty fun!
    - Also used some ES6 goodness

--- client/js/PostList.js
+++ client/js/PostList.js
@@ -1,24 +1,20 @@
-var Post = require('./Post');
-var React = require('react');
+let Post = require('./Post');
+let React = require('react');
 
-var PostList = React.createClass({
-	render: function () {
-		var nodes = this.props.posts.map(function (post) {
-			return (
-				<li key={post.name}
-					className={post.isLoading ? 'is-loading' : ''}
-					onClick={this.props.handlePostClicked.bind(null, post)}>
-					<Post title={post.title} date={post.date} />
-				</li>
-			);
-		}.bind(this));
+function PostList(props) {
+	let nodes = props.posts.map(post => (
+		<li key={post.name}
+			className={post.isLoading ? 'is-loading' : ''}
+			onClick={props.handlePostClicked.bind(null, post)}>
+			<Post title={post.title} date={post.date} />
+		</li>
+	));
 
-		return (
-			<div id="post-list">
-				<ul className="list-boxes">{nodes}</ul>
-			</div>
-		);
-	}
-});
+	return (
+		<div id="post-list">
+			<ul className="list-boxes">{nodes}</ul>
+		</div>
+	);
+}
 
 module.exports = PostList;
